dd06329519b6e0b8d4c19fc9cf4136681c701663
masrudmubarok <masrud.mubarok21@gmail.com>
2025-03-08 22:32:34 +0700

    Intial setup

---
+++ app/Database/Migrations/2025-03-08-create_events_table.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateEventsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+            ],
+            'date' => [
+                'type' => 'DATE',
+            ],
+            'location' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('events');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('events');
+    }
+}
